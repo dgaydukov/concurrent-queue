c2d46e679cb6ce65f0277530f68b20e2329f4158
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-04-07 11:23:07 +0300

    add single-threaded test cases for non-blocking queues

---
+++ src/test/java/com/java/queue/nonblocking/SingleThreadCorrectnessTest.java
@@ -0,0 +1,71 @@
+package com.java.queue.nonblocking;
+
+import com.java.queue.interfaces.Queue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * This test cases designed to test correctness of non-blocking queues for single threaded application
+ * we don't test multi-threaded logic here, just is the queue correctly written for single thread environment
+ */
+public class SingleThreadCorrectnessTest {
+
+    public static Stream<Queue<String>> getQueues() {
+        return Stream.of(new OneToOneBoundedArrayQueue<>(10));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getQueues")
+    public void testAddItems(Queue<String> queue){
+        Assertions.assertEquals(0, queue.getSize(), "size should be 0");
+        Assertions.assertNull(queue.poll(), "queue should be empty");
+        for (int i = 0; i < 10; i++){
+            Assertions.assertTrue(queue.offer("msg_"+i), "should add message successfully");
+        }
+        Assertions.assertEquals(10, queue.getSize(), "size should be 10");
+        // check that can't add any more items
+        for (int i = 0; i < 10; i++){
+            Assertions.assertFalse(queue.offer("msg_"+i), "should not be able to add items");
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getQueues")
+    public void testPollItems(Queue<String> queue){
+        Assertions.assertNull(queue.poll(), "queue should be empty");
+        for (int i = 0; i < 10; i++){
+            queue.offer("msg_"+i);
+        }
+        for (int i = 0; i < 10; i++){
+            Assertions.assertEquals("msg_"+i, queue.poll(), "polled item mismatch");
+        }
+        Assertions.assertNull(queue.poll(), "queue should be empty");
+        Assertions.assertEquals(0, queue.getSize(), "size should be 0");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getQueues")
+    public void testAddAndPoll(Queue<String> queue){
+        Assertions.assertEquals(0, queue.getSize(), "size should be 0");
+        for (int i = 0; i < 10; i++){
+            Assertions.assertTrue(queue.offer("msg_"+i), "should add message successfully");
+        }
+        Assertions.assertEquals(10, queue.getSize(), "size should be 10");
+        for (int i = 0; i < 5; i++){
+            Assertions.assertEquals("msg_"+i, queue.poll(), "polled item mismatch");
+        }
+        Assertions.assertEquals(5, queue.getSize(), "size should be 5");
+        for (int i = 10; i < 15; i++){
+            Assertions.assertTrue(queue.offer("msg_"+i), "should add message successfully");
+        }
+        Assertions.assertEquals(10, queue.getSize(), "size should be 10");
+        for (int i = 5; i < 15; i++){
+            Assertions.assertEquals("msg_"+i, queue.poll(), "polled item mismatch");
+        }
+        Assertions.assertEquals(0, queue.getSize(), "size should be 0");
+        Assertions.assertNull(queue.poll(), "queue should be empty");
+    }
+}
